82a0467c7ca9421e3727bf5796b90a46a0573001
TechBuddy <122123938+AliRasikh@users.noreply.github.com>
2024-09-29 15:30:26 +0200

    pokemon project completed

--- fetch_PokeApi_find_ur_fav_pokemon/index.js
+++ fetch_PokeApi_find_ur_fav_pokemon/index.js
@@ -1,9 +1,10 @@
 const fetchBtn = document.getElementById("fetch--btn");
 let pokemonName = document.getElementById("pokemon--name");
+const pokemonImg = document.getElementById("pokemon--img--container");
+const pokemonData = document.getElementById("pokemon--data--container");
 
 fetchBtn.addEventListener("click", () => {
   const pokemon = pokemonName.value;
-  console.log(pokemon);
   try {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
       .then((response) => {
@@ -15,7 +16,34 @@ fetchBtn.addEventListener("click", () => {
       .then((data) => {
         const img = document.createElement("img");
         img.src = data.sprites.front_default;
-        test.appendChild(img);
+        pokemonImg.innerHTML = "";
+        pokemonImg.appendChild(img);
+        const name = document.createElement("h3");
+        name.textContent = data.name;
+        const height = document.createElement("p");
+        height.textContent = `Height: ${data.height}`;
+        const weight = document.createElement("p");
+        weight.textContent = `Weight: ${data.weight}`;
+        const abilities = document.createElement("p");
+        abilities.textContent = `Abilities: ${data.abilities
+          .map((ability) => ability.ability.name)
+          .join(", ")}`;
+        const types = document.createElement("p");
+        types.textContent = `Types: ${data.types
+          .map((type) => type.type.name)
+          .join(", ")}`;
+        const moves = document.createElement("p");
+        moves.textContent = `Moves: ${data.moves
+          .map((move) => move.move.name)
+          .slice(0, 5)
+          .join(", ")}`;
+        pokemonData.innerHTML = "";
+        pokemonData.appendChild(name);
+        pokemonData.appendChild(height);
+        pokemonData.appendChild(weight);
+        pokemonData.appendChild(abilities);
+        pokemonData.appendChild(types);
+        pokemonData.appendChild(moves);
       })
       .catch((error) => {
         console.error("Error fetching the Pokémon data:", error);
